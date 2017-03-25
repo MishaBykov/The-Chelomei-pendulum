680cd30d33a987cd90ca0639cb0f78a5c865e60c
mishabykov <mike008@bk.ru>
2017-03-25 21:08:31 +0300

    +

--- src/Setting.java
+++ src/Setting.java
@@ -1,4 +1,5 @@
 import java.awt.geom.Point2D;
+import java.util.HashMap;
 
 public class Setting {
     private static double widthWasher = 0.1;
@@ -27,5 +28,25 @@ public class Setting {
     public static Point2D.Double findTwoPoint(Point2D.Double onePoin, double len, double angle) {
         return new Point2D.Double(-Math.sin(angle) * len + onePoin.getX(),
                 Math.cos(angle) * len + onePoin.getY());
+    }
+    
+    public static HashMap<String, Double> getScaleSlider(){
+        HashMap<String, Double> result = new HashMap<String, Double>();
+        result.put("x", 1.0);
+        result.put("phi", 1.0);
+        result.put("dotX", 1.0);
+        result.put("dotPhi", 1.0);
+        result.put("I1", 1.0);
+        result.put("I2", 1.0);
+        result.put("m", 1.0);
+        result.put("L", 1.0);
+        result.put("k1", 1.0);
+        result.put("k2", 1.0);
+        result.put("M", 1.0);
+        result.put("g", 1.0);
+        result.put("alpha", 1.0);
+        result.put("nu", 1.0);
+        result.put("theta", 1.0);
+        return result;
     }
 }
