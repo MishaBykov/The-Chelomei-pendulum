4836705e80689d01226478c07f71d0e895dcabed
mishabykov <mike008@bk.ru>
2017-04-18 16:49:59 +0300

    +

--- src/CrashSystem.java
+++ src/CrashSystem.java
@@ -4,10 +4,12 @@ import java.util.Map;
 
 
 public class CrashSystem {
-    String nameSystem = "system";
-    String nameWasher = "washer";
+    private final String nameSystem = "system";
+    private final String nameWasher = "washer";
 
     private Values values;
+    private Map<String, Double> variablesSystem;
+    private Map<String, Double> variablesWasher;
     private Map<String, Double> parameters;
     private Double x;
     private Double l;
@@ -15,21 +17,28 @@ public class CrashSystem {
 
     public CrashSystem(Values values) {
         this.values = values;
+        variablesSystem = values.getVariables(nameSystem);
+        variablesWasher = values.getVariables(nameWasher);
         parameters = values.getParameters();
         x = values.getVariables(nameSystem).get("x");
         l = values.getParameters().get("l");
         crashSystem = l < x;
     }
 
-    public void systemToCrash(List<RK4> rk4List){
+    public void systemToCrash(List<RK4> rk4List) {
         double t = rk4List.get(0).getT();
+        double angle = variablesSystem.get("phi");
         Point2D.Double pointWasher = Config.findTwoPoint(
                 SuspensionPoint.get(parameters, t),
                 x + Config.getHeightWasher() / 2,
-                values.getVariables(nameSystem).get("phi")
+                angle
         );
-        values.getVariables(nameWasher).put("x", pointWasher.getX());
-        values.getVariables(nameWasher).put("y", pointWasher.getY());
+        variablesWasher.put("x", pointWasher.getX());
+        variablesWasher.put("y", pointWasher.getY());
+        double v = variablesSystem.get("dotPhi") * l;
+        variablesWasher.put("dotX", v * Math.cos(angle));
+        variablesWasher.put("dotY", v * Math.cos(angle));
+
         Functions WF = new WasherFunctions(values);
         rk4List.add(new RK4(WF, values, t, Config.getStep()));
         values.getParameters().put("m", 0.0);
